a94aef5797f84ff2504438a2ff44950de3c17c06
Jusamua <156001818+Jusamua@users.noreply.github.com>
2024-01-31 20:20:18 +0200

    JP commit

--- deals_comparison.py
+++ deals_comparison.py
@@ -9,7 +9,7 @@ df1 = pd.read_csv('csv/makalius.csv')
 df1['Provider'] = 'Makalius'
 df1['Date'] = df1['Date'].str.replace('Data: ', '').str.split().str[0]
 df1['Hotel Stars'] = df1['Hotel Stars'].str.replace(' žvaigždučių viešbutis', '')
-df2 = pd.read_csv('csv/novaturas.csv')
+df2 = pd.read_csv('csv/makalius.csv')
 df2['Provider'] = 'Novaturas'
 df3 = pd.read_csv('csv/westexpress.csv')
 df3['Provider'] = 'WestExpress'
@@ -23,3 +23,25 @@ df['Hotel Stars'] = pd.to_numeric(df['Hotel Stars'])
 # df['Country'] = df['Destination'].str.replace(',', '').str.replace('.', '').str.split().str[0]
 df['Country'] = df['Destination'].apply(lambda x: x.split()[0] if x != 'Žaliasis Kyšulys' else x)
 df.to_csv('csv/deals_comparison.csv', index=False)
+
+# Grouping Price averages by Providers
+price_avg_per_provider = df.groupby('Provider')['Price'].mean()
+
+# Creating barplot for Average price by Provider
+plt.figure(figsize=(12, 8))
+price_avg_per_provider.plot(kind='bar')
+plt.title('Last minute average trip price by Provider')
+plt.xlabel('Provider')
+plt.ylabel('Price')
+plt.show()
+
+# Grouping Price averages by Countries
+price_avg_per_country = df.groupby('Country')['Price'].mean()
+
+# Creating barplot for Average price by Country
+plt.figure(figsize=(12, 8))
+price_avg_per_country.plot(kind='bar')
+plt.title('Last minute average trip price by Country')
+plt.xlabel('Country')
+plt.ylabel('Price')
+plt.show()
